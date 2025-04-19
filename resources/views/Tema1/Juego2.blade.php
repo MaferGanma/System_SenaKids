4e540eaaf91b585590070b4068e65fac802451ee
MaferGanma <maferganma210601@gmail.com>
2025-04-18 21:36:09 -0400

    Mis cambios locales antes del pull

--- resources/views/Tema1/Juego2.blade.php
+++ resources/views/Tema1/Juego2.blade.php
@@ -1,190 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Memograma Parejas</title>
-    <link rel="stylesheet" href="{{asset('css/stylememograma.css')}}" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-  </head>
-  <body>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <div class="container">
-        <div class="floating-stars">
-            <span class="star" style="top:10%; left:5%;">✨</span>
-            <span class="star" style="top:20%; left:90%; animation-delay:0.5s;">⭐</span>
-            <span class="star" style="top:70%; left:10%; animation-delay:1s;">🌟</span>
-            <span class="star" style="top:80%; left:80%; animation-delay:1.5s;">✨</span>
-          </div>
-
-          <!-- Título y encabezado -->
-          <div class="title-container">
-            <h1 class="game-title">Memograma Mágico</h1>
-          </div>
-
-          {{-- <div class="score-container">
-            <span class="score-text">Parejas: <span class="score">0</span> de <span class="attempts" id="intentos">0</span> intentos</span>
-            <button class="pulse" id="escuchar">Sonido: ON</button>
-          </div> --}}
-          <div class="score-container">
-            <h2>Parejas <span>0</span> de  <span id="intentos">0</span> intentos
-              <button class="pulse" id="escuchar">Sonido: ON</button>
-            </h2>
-        </div>
-
-      <div class="cards">
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/1.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/2.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/3.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/4.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/5.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/6.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/7.jpg')}}" alt="" />
-          </div>
-        </li>
-        <li class="card">
-          <div class="view front-view">
-            <span class="material-icons"> question_mark </span>
-          </div>
-          <div class="view back-view">
-            <img src="{{asset('/images/memograma/8.jpg')}}" alt="" />
-          </div>
-        </li>
-
-
-        <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/1.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/2.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/3.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/4.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/5.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/6.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/7.jpg')}}" alt="" />
-            </div>
-          </li>
-          <li class="card">
-            <div class="view front-view">
-              <span class="material-icons"> question_mark </span>
-            </div>
-            <div class="view back-view">
-              <img src="{{asset('/images/memograma/8.jpg')}}" alt="" />
-            </div>
-          </li>
-      </div>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Memograma Imagen - Palabra</title>
+  <link rel="stylesheet" href="stylememograma.css" /> 
+</head>
+<body>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <div class="container">
+    <div class="title-container">
+      <h1 class="game-title">Memorama Mágico: Imagen y Palabra</h1>
     </div>
 
-    <div id="modalResultados" class="modal">
-        <div class="modal-content">
-          <h2>¡Felicidades! 🎉</h2>
-          <p>Has completado el juego en <span id="totalIntentos"></span> intentos.</p>
-          <button id="btnReintentar">Intentar de nuevo</button>
-          <button id="btnSiguiente">Siguiente</button>
-        </div>
-      </div>
+    <div class="score-container">
+      <h2>Parejas <span>0</span> de <span id="intentos">0</span> intentos
+        <button class="pulse" id="escuchar">Sonido: ON</button>
+      </h2>
+    </div>
 
-      <input type="hidden" id="userId" value="{{ Auth::id() }}">
+    <ul class="cards"></ul>
+  </div>
 
-      <button onclick="guardarPuntaje(100)" style="display: none;">Presionar</button>
+  <div id="modalResultados" class="modal">
+    <div class="modal-content">
+      <h2>¡Felicidades! 🎉</h2>
+      <p id="totalIntentos"></p>
+      <button id="btnReintentar">Intentar de nuevo</button>
+      <button id="btnSiguiente">Siguiente</button>
+    </div>
+  </div>
 
-    <audio id="fondo" loop src="{{asset('sounds/musica.mp3')}}" type="audio/mpeg"/>
-    <audio id="sonidos" src="" type="audio/mpeg"/>
-    <script src="{{asset('js/scriptmemograma.js')}}"></script>
+  <input type="hidden" id="userId" value="{{ Auth::id() }}">
 
-  </body>
+  <audio id="fondo" loop src="sounds/musica.mp3" type="audio/mpeg"></audio>
+  <audio id="sonidos" src="" type="audio/mpeg"></audio>
+  <script src="scriptmemograma.js"></script>
+</body>
 </html>
